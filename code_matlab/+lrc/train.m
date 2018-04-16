3fc0f1154e959e7fe17b8ede83a017866fb53526
telec16 <diode@sfr.fr>
2018-04-16 20:11:57 +0200

    Improved test script

--- code_matlab/+lrc/train.m
+++ code_matlab/+lrc/train.m
@@ -1,13 +1,17 @@
-function [all_theta] = train(X, y, num_labels, maxIter, epsilon, tau, methode) %methode=0 si gradient simple, 1 si conjugue
+function [all_theta] = train(X, y, num_labels, maxIter, epsilon, tau, approach) 
 
     %-- [all_theta] = train(X, y, num_labels, maxIter, epsilon, tau)
     %-- Trains multiple logistic regression classifiers and returns all
     %-- the classifiers in a matrix all_theta, where the i-th row of all_theta 
     %-- corresponds to the classifier for label i
+    %-- approach can be "conjugate" or "simple" (default)
 
+    if (~exist('approach','var'))
+        approach = "simple";
+    end
     
     %-- Get the number of samples m and the size of the parameter vector
-    [m,n] = size(X);
+    [~,n] = size(X);
 
     %-- Loop over each class
     all_theta = zeros(num_labels,n);
@@ -20,10 +24,14 @@ function [all_theta] = train(X, y, num_labels, maxIter, epsilon, tau, methode) %
         
         %-- Run gradient descent method to update theta values
         options = struct('MaxIter',maxIter,'epsilon',epsilon,'tau',tau);
-        if(methode) %pour partie comparaison
-            [theta] = lrc.conjugate_gradient(@(t)(lrc.lrCostFunction(t, X, (y == c-1))), initial_theta, options);
+        if(approach == "conjugate")
+            [theta] = lrc.conjugate_gradient( ...
+            @(t)(lrc.lrCostFunction(t, X, (y == c-1))), ...
+            initial_theta, options);
         else
-            [theta] = lrc.gradient_descent(@(t)(lrc.lrCostFunction(t, X, (y == c-1))), initial_theta, options);
+            [theta] = lrc.gradient_descent( ...
+            @(t)(lrc.lrCostFunction(t, X, (y == c-1))), ...
+            initial_theta, options);
         end
         
         %-- Store corresponding result into all_theta matrix
--- code_matlab/scripts/scriptInfluenceMaxIteration.m
+++ code_matlab/scripts/scriptInfluenceMaxIteration.m
@@ -1,7 +1,17 @@
-clear all;
+clear;
 close all;
 clc;
 addpath('../');
+
+
+%-- parameters
+fromIter = 20;  %-- gradient descent: start number of iterations
+stepsIter = 10; %-- gradient descent: step number of iterations
+toIter = 200;   %-- gradient descent: end number of iterations
+epsilon = 0;    %-- gradient descent: convergence thresholder
+tau = 1;        %-- gradient descent: learning rate coefficient
+methods = ["simple", "conjugate"];
+ 
 
 %-- mnist database location
 url = 'https://www.creatis.insa-lyon.fr/~bernard/ge/';
@@ -20,58 +30,44 @@ load([local_data_path,filename_db]);
 widthDigit = size(training.images,2);
 heightDigit = size(training.images,1);
 
-%-- Create X matrix for train
-X = zeros(size(training.images,3),widthDigit*heightDigit+1);
-for k=1:size(training.images,3)
-    digit = training.images(:,:,k);
+
+num_labels = 10;          %-- 10 labels, from 0 to 9
+
+
+
+%-- Create X matrix
+X = zeros(size(test.images,3),widthDigit*heightDigit+1);
+for k=1:size(test.images,3)
+    digit = test.images(:,:,k);
     X(k,:) = [1,digit(:)'];
 end
 
 
-%-- Create y vector for train
-y = training.labels;
+%-- Create y vector
+y = test.labels;
 m = size(X,1);
 
-%-- Create X matrix for test
-Xt = zeros(size(test.images,3),widthDigit*heightDigit+1);
-for k=1:size(test.images,3)
-    digit = test.images(:,:,k);
-    Xt(k,:) = [1,digit(:)'];
-end
-yt = test.labels;
-m = size(Xt,1);
 
-%Number of labels
-num_labels = 10; 
-
-%--  train logistic regression method -> gradient descent
-disp('\nTraining Logistic Regression with simple gradient descent methode depending on iteration number...\n')
-tau=1;
-epsilon=0;
-methode = 0;
-accuracy_gradient_descent=zeros(1,10);
-for i=1:10
-    maxIter=20+(i-1)*20;
-    [all_theta] = lrc.train(X, y, num_labels, maxIter, epsilon, tau, methode);
-    pred = lrc.predict(all_theta, Xt);
-    accuracy_gradient_descent(i)= mean(double(pred == yt) * 100);
-end
-
-%--  train logistic regression method -> conjugate gradient
-disp('\nTraining Logistic Regression with conjugate gradient depending on iteration number...\n')
-tau=1;
-epsilon=0;
-methode = 1;
-accuracy_conjugate_gradient=zeros(1,10);
-for i=1:10
-    maxIter=20+(i-1)*20;
-    [all_theta] = lrc.train(X, y, num_labels, maxIter, epsilon, tau, methode);
-    pred = lrc.predict(all_theta, Xt);
-    accuracy_conjugate_gradient(i)= mean(double(pred == yt) * 100);
-end
-
+%--  train logistic regression method
 figure;
-plot(20:18:200,accuracy_gradient_descent,'c');
 hold on;
-plot(20:18:200,accuracy_conjugate_gradient,'b');
+accuracies = struct;
+for method = methods
+    fprintf('\n\n\nTraining Logistic Regression with %s gradient descent methode depending on iteration number...\n\n\n', method);
 
+    accuracy=zeros(1,10);
+    for i=1:stepsIter
+        maxIter=fromIter+(i-1)*((toIter-fromIter)/(stepsIter-1));
+        fprintf('\n\nCurrently: %4i\n\n', num2str(maxIter));
+        
+        [all_theta] = lrc.train(X, y, num_labels, maxIter, epsilon, tau, method);
+        pred = lrc.predict(all_theta, X);
+        accuracy(i) = mean(double(pred == y) * 100);
+    end
+    
+    accuracies.(method) = accuracy;
+    plot(accuracy);
+end
+hold off;
+
+fprintf('\n\n\nEnd. Thank you.');
